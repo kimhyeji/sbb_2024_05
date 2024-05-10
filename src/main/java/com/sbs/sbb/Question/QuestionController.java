fef72b2b18e4009d5ad5338ff13f2b30d874005b
kimhyeji <gpwl5589@gmail.com>
2024-05-10 14:41:05 +0900

    QuestionForm 바인딩 에러 발생 시 html로 응답

--- src/main/java/com/sbs/sbb/Question/QuestionController.java
+++ src/main/java/com/sbs/sbb/Question/QuestionController.java
@@ -4,6 +4,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -40,7 +41,13 @@ public class QuestionController {
 
     @PostMapping("/create")
     // QuestionForm 값을 바인딩 할 때 유효성 체크를 해라!
-    public String questionCreate(@Valid QuestionForm questionForm) {
+    public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if ( bindingResult.hasErrors() ) {
+            // question_form.html 실행
+            // 다시 작성하라는 의미로 응답에 폼을 싫어서 보냄
+            return "question_form";
+        }
+        
         Question q = this.questionService.create(questionForm.getSubject(), questionForm.getSubject());
 
         return "redirect:/question/list";
