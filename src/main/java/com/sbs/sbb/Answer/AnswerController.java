19e4a6dd66ed6fddfe5e9ff6eb7775a970aef078
kimhyeji <gpwl5589@gmail.com>
2024-05-14 15:14:01 +0900

    Answer, Question 엔티티에 작성자 필드 추가

--- src/main/java/com/sbs/sbb/Answer/AnswerController.java
+++ src/main/java/com/sbs/sbb/Answer/AnswerController.java
@@ -3,6 +3,8 @@ package com.sbs.sbb.Answer;
 import com.sbs.sbb.Question.Question;
 import com.sbs.sbb.Question.QuestionRepository;
 import com.sbs.sbb.Question.QuestionService;
+import com.sbs.sbb.user.SiteUser;
+import com.sbs.sbb.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -13,12 +15,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.security.Principal;
+
 @RequestMapping("/answer")
 @Controller
 @RequiredArgsConstructor
 public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
+    private final UserService userService;
+
 
     @PostMapping("/create/{id}")
     public String createAnswer(
@@ -26,18 +32,20 @@ public class AnswerController {
             @PathVariable("id")
             Integer id,
             @Valid AnswerForm answerForm,
-            BindingResult bindingResult
+            BindingResult bindingResult,
+            Principal principal
             ) {
 
         // 답변 부모 질문객체를 받아온다.
         Question q = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
 
         if ( bindingResult.hasErrors() ) {
             model.addAttribute("question", q);
             return "question_detail";
         }
 
-        Answer answer = this.answerService.create(q, answerForm.getContent());
+        Answer answer = this.answerService.create(q, answerForm.getContent(), siteUser);
 
         return "redirect:/question/detail/%d".formatted(id);
     }
